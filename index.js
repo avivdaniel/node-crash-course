cbd98435aa2cffcfe0df2a62ffede20b10b380fe
Aviv Daniel <avivdaniel@gmail.com>
2021-08-28 23:14:47 +0300

    Remove ninja

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes/api');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // express index - instance;
 const app = express();
@@ -8,9 +9,28 @@ const app = express();
 app.use(bodyParser.json());
 
 //Initialized routes
-app.use('/api',routes);
+app.use('/api', routes);
+
+//Error handling middleware
+app.use((err, req, res, next)=> {
+    res.status(422).send({error: err.message})
+});
+
+connect();
 
 // listen to requests;
-app.listen(process.env.port || 3000, ()=> {
-    console.log('App listen to port 3000');
-});
+function listen() {
+    app.listen(process.env.port || 3000, () => {
+        console.log('App listen to port 3000');
+    })
+};
+
+//Connect to db
+function connect() {
+    mongoose.connect('mongodb://localhost:27017/ninjago', {
+        useNewUrlParser: true
+    });
+    const db = mongoose.connection;
+    db.on('error', err => console.log(err));
+    db.once('open', listen);
+}
--- routes/api.js
+++ routes/api.js
@@ -1,29 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const Ninja = require('../models/ninja');
 
 //Get a list of ninjas from db
-router.get('/ninjas', (req, res) => {
+router.get('/ninjas', (req, res, next) => {
     res.send({type: 'GET'})
 });
 
 //ADD
-router.post('/ninjas', (req, res) => {
-    res.send(
-        {
-            type: 'POST',
-            name: req.body.name
-        }
-    )
+router.post('/ninjas', async (req, res, next) => {
+    try {
+        const ninja = await Ninja.create(req.body);
+        res.send(ninja);
+    } catch (err) {
+        next(err);
+    }
 });
 
+
 //UPDATE
-router.put('/ninjas/:id', (req, res) => {
+router.put('/ninjas/:id', (req, res, next) => {
     res.send({type: 'PUT'})
 });
 
 //DELETE
-router.delete('/ninjas/:id', (req, res) => {
-    res.send({type: 'DELETE'})
+router.delete('/ninjas/:id', async (req, res, next) => {
+    try {
+    const ninja = await Ninja.findByIdAndRemove({
+        _id: req.params.id
+    });
+    res.send(ninja);
+    } catch (err) {
+        next(err);
+    }
 });
 
 module.exports = router;
